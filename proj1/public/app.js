706c2775bd7771d71c87baa5fa6406773c1b7890
Artis Ademi <Artis@basecodes-iMac.local>
2023-09-04 16:58:01 +0200

    Work until video 18 from playlist

--- proj1/public/app.js
+++ proj1/public/app.js
@@ -1,4 +1,28 @@
-"use strict";
+import { Invoice } from "./models/invoice.js";
+import { Payment } from "./models/payment.js";
+import { ListTemplates } from "./models/listTemplates";
+let docOne;
+let docTwo;
+docOne = new Invoice("Arianit", "Web Work", 1000);
+docTwo = new Payment("leona", "Flutter Work", 1500);
+let docs = [];
+docs.push(docOne, docTwo);
+console.log(docs);
+const me = {
+    name: "Artis",
+    age: 21,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent 0', amount);
+        return amount;
+    },
+};
+const greetPerson = (p) => {
+    console.log('Hello ', p.name);
+};
+greetPerson(me);
 const anchor = document.querySelector('a');
 console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
 // const form = document.querySelector('form')!;
@@ -9,7 +33,26 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template
+const ul = document.querySelector('ul');
+const list = new ListTemplates(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+});
+// Classes
+const invOne = new Invoice('Artis', 'Work on Artis Website', 400);
+const invTwo = new Invoice('Labi', 'Work on Labi Website', 500);
+let invoices = [];
+invoices.push(invOne, invTwo);
+// console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.details, inv.format());
 });
--- proj1/src/app.ts
+++ proj1/src/app.ts
@@ -1,3 +1,47 @@
+import { Invoice } from "./models/invoice.js";
+import { Payment } from "./models/payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplates } from "./models/listTemplates";
+
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice("Arianit", "Web Work", 1000);
+docTwo = new Payment("leona", "Flutter Work", 1500);
+
+let docs: HasFormatter[] = [];
+docs.push(docOne, docTwo);
+
+console.log(docs);
+
+//  interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: "Artis",
+    age:21,
+    speak(text: string): void{
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent 0', amount);
+        return amount;
+    },
+}
+
+const greetPerson = (p: IsPerson) => {
+    console.log('Hello ', p.name);
+}
+
+greetPerson(me);
+
+
+
 const anchor = document.querySelector('a');
 
 console.log(anchor?.href);   
@@ -13,13 +57,33 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template
+const ul = document.querySelector('ul')!;
+const list = new ListTemplates(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber,
-        );
+    let doc: HasFormatter; 
+    if(type.value === 'invoice'){
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+
+    }else{
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+    
 })
+
+// Classes
+
+const invOne = new Invoice('Artis', 'Work on Artis Website', 400);
+const invTwo = new Invoice('Labi', 'Work on Labi Website', 500);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne, invTwo);
+// console.log(invoices);
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.details, inv.format());
+});
